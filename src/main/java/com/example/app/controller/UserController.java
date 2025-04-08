3eb6fbb566e9bd6d8a3974b89c3d46709f7a96ae
mohammad <141648080+micron-group@users.noreply.github.com>
2025-04-09 00:21:01 +0600

    Add src/main/java/com/example/app/controller/UserController.java 1744136456420

---
+++ src/main/java/com/example/app/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.example.app.controller;
+
+import com.example.app.model.User;
+import com.example.app.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+}
